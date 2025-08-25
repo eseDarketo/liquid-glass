311c3394468b8520986052b3700a9f6163422aec
isaacMagma <isaac@magmapartners.com>
2025-08-25 17:57:28 -0600

    new draggable card

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import GlassCard from "@/components/GlassCard";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <div className={styles.dots}>
-
+        <GlassCard>
+          <h1>Hello</h1>
+        </GlassCard>
       </div>
     </div>
   );
